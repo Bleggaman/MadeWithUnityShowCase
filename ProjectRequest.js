a1c5dd9cc99616c73357fca2fa891e92c4b535fd
Britt Henderson <Bleggaman@yahoo.com>
2018-11-07 22:07:10 -0800

    working to test ajax calls

--- ProjectRequest.js
+++ ProjectRequest.js
@@ -7,6 +7,12 @@ function ChangePage() {
     console.log("Bleg 1");
     
     var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+            this.responseText;
+       }
+    };
     xhttp.open("GET", "RequestPage.aspx", true);
     xhttp.send();
     console.log("Bleg 2");
