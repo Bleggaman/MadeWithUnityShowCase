87f3a5e3957e138690fb0b9a12f7ec8cd8f820b6
Britt Henderson <Bleggaman@yahoo.com>
2018-11-08 15:39:22 -0800

    about to try adding asp.net

--- ProjectRequest/ProjectRequest.js
+++ ProjectRequest/ProjectRequest.js
@@ -13,7 +13,7 @@ function ChangePage() {
             document.getElementById("Bleg").innerHTML = "<h2>" + this.responseText + "</h2>";
        }
     };
-    xhttp.open("GET", "RequestPage.cshtml", true);
+    xhttp.open("GET", "RequestProject.cshtml", true);
     xhttp.send();
     console.log("Bleg 3");
 }
